7d10411c47fd47c21e2d7f815869d8741dcaa703
toweringpillow <toweringpillow@vulnerabilityhub.com>
2025-11-23 14:48:27 -0500

    Add: Simple test page to verify routing works

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function TestPage() {
+  return (
+    <div className="min-h-screen bg-dark-900 text-white p-8">
+      <h1 className="text-4xl font-bold mb-4">Test Page</h1>
+      <p className="text-xl">If you can see this, routing is working!</p>
+      <p className="mt-4">This page doesn't use Supabase, so it should always load.</p>
+    </div>
+  )
+}
+
