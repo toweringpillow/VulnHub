f81c2cb3225beb4e3e6d41b35514868bd3ab64aa
toweringpillow <toweringpillow@vulnerabilityhub.com>
2025-11-23 15:42:37 -0500

    Add: Minimal test page without layout to test routing

---
+++ app/minimal/page.tsx
@@ -0,0 +1,12 @@
+// Minimal test page that doesn't use layout
+export default function MinimalPage() {
+  return (
+    <html>
+      <body>
+        <h1>Minimal Test Page</h1>
+        <p>If you see this, routing works!</p>
+      </body>
+    </html>
+  )
+}
+
